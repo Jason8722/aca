85ca6705ae3a62b16edc297ac1bdf68ca846e6e7
Jason <99275134+Jason8722@users.noreply.github.com>
2022-07-28 00:18:23 +0900

    345

---
+++ JavaScript_0719~0725/4일차/jstest.js
@@ -0,0 +1,9 @@
+        /* window.alert("안녕하세요");  window객체는 생략 가능*/
+        // alert("아녕");
+        // document.write("안녕");
+        document.write('"안녕"하세요');
+        document.write("'안녕'하세요");
+        document.write("\"안녕\"하세요"); //쌍따옴표 그대로 표시하고 싶을 때 역슬래시를 앞에 붙여줌
+        console.log("콘솔창에 이렇게 결갓값을 띄울 수 있습니다.");
+        console.log(10+3);
+        console.log("10+3");
